dc9f7dc706d0cb71734b5de57c396917c1861e8a
roberto <robypriz@gmail.com>
2021-03-05 11:18:50 +0100

    Extract Interface: with Mockito
    Create an interface for a dependency, allowing a test double to implement that interface

--- src/test/java/erserver/modules/dependencies/StaffAssignmentManagerTest.java
+++ src/test/java/erserver/modules/dependencies/StaffAssignmentManagerTest.java
@@ -1,7 +1,10 @@
 package erserver.modules.dependencies;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoRule;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -14,6 +17,21 @@ public class StaffAssignmentManagerTest {
         stafProviderDouble.addStaff(new Staff(1, "rob", StaffRole.DOCTOR));
         stafProviderDouble.addStaff(new Staff(2, "ale", StaffRole.RESIDENT));
         StaffAssignmentManager manager = new StaffAssignmentManager(stafProviderDouble, new BedProviderDouble());
+        List<Staff> physiciansOnDuty = manager.getPhysiciansOnDuty();
+        assertEquals(2, physiciansOnDuty.size());
+        assertEquals(1, physiciansOnDuty.get(0).getStaffId());
+        assertEquals(2, physiciansOnDuty.get(1).getStaffId());
+    }
+
+    @Test
+    public void withMockito(){
+        List<Staff> docsAndResidents = new ArrayList<>();
+        docsAndResidents.add(new Staff(1, "rob", StaffRole.DOCTOR));
+        docsAndResidents.add(new Staff(2, "ale", StaffRole.RESIDENT));
+        StaffProvider staffProviderMock = Mockito.mock(StaffProvider.class);
+        BedProvider bedProviderMock = Mockito.mock(BedProvider.class);
+        Mockito.when(staffProviderMock.getShiftStaff()).thenReturn(docsAndResidents);
+        StaffAssignmentManager manager = new StaffAssignmentManager(staffProviderMock, bedProviderMock);
         List<Staff> physiciansOnDuty = manager.getPhysiciansOnDuty();
         assertEquals(2, physiciansOnDuty.size());
         assertEquals(1, physiciansOnDuty.get(0).getStaffId());
